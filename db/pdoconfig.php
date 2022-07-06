88452f21c594f7f05e43c9464f6d502cfac59010
Edu Albalat <ealbalat@CLMAC0014.local>
2022-07-06 16:47:02 +0200

    actualizado

--- db/pdoconfig.php
+++ db/pdoconfig.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = 'nuepp3ddzwtnggom.chr7pe7iynqr.eu-west-1.rds.amazonaws.com';
-$dbname = 'fueilo0gl2qjcomo';
-$username = 'zd99lu0rjh35qzml';
-$password = 'am4lre9g1zus0vjq';
+$dbname = 'ccjeawoyb5ao49bu';
+$username = 'ehj6w7553bk6ymlb';
+$password = 'lq1fknc6jt6aqlvt';
 
 $options = [
     \PDO::ATTR_ERRMODE  => \PDO::ERRMODE_EXCEPTION,
